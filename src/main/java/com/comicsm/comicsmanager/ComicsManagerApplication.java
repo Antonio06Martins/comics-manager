bbca672286437f363bb319dd4f6e2f94159ce5f2
Antonio06Martins <toni6martins@gmail.com>
2021-07-29 17:56:32 -0300

    create comics manager

---
+++ src/main/java/com/comicsm/comicsmanager/ComicsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.comicsm.comicsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comicsm/comicsmanager/ComicsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comicsm.comicsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
